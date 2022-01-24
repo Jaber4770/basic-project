b1e904af9e16cc835e489b1ce936490098ee9074
jaber4770 <jaber.ahmed4770@gmail.com>
2022-01-24 22:18:15 +0600

    added private router

--- src/component/privateRoute/PrivateRoute.js
+++ src/component/privateRoute/PrivateRoute.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 import UseAuth from '../Hooks/UseAuth';
 
 const PrivateRoute = ({ children, ...rest }) => {
     const { user } = UseAuth();
-return (
-    <Route
-        {...rest}
-        render={({ location }) => user}
-    >
+    return (
+        <Route
+            {...rest}
+            render={({ location }) => user.email ? children : <Redirect
+                to={{
+                    pathname: '/login',
+                    state: { from: location }
+                }}
+            >
+            </Redirect>}
+        >
 
-    </Route>
-);
+        </Route>
+    );
 };
 
 export default PrivateRoute;
